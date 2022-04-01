e0b27061e82415b3b45bef04e92827e2ed396260
ardhaniahlan <ardhan.official16@gmail.com>
2022-04-01 13:08:45 +0700

    view Binding

--- app/src/main/java/org/d3if0106/bangundatar1/MainActivity.kt
+++ app/src/main/java/org/d3if0106/bangundatar1/MainActivity.kt
@@ -2,10 +2,17 @@ package org.d3if0106.bangundatar1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.d3if0106.bangundatar1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }

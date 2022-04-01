a94409f1a038ebd9f33b1ff60ca2d9d7dba777e7
ardhaniahlan <ardhan.official16@gmail.com>
2022-04-01 13:32:23 +0700

    Logic perhitungan bangundatar

--- app/src/main/java/org/d3if0106/bangundatar1/MainActivity.kt
+++ app/src/main/java/org/d3if0106/bangundatar1/MainActivity.kt
@@ -2,6 +2,8 @@ package org.d3if0106.bangundatar1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import org.d3if0106.bangundatar1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,24 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnHitung.setOnClickListener{
+            keliling()
+        }
+    }
+
+    private fun keliling(){
+
+        val jari = binding.jarijari.text.toString()
+        if (TextUtils.isEmpty(jari)){
+            Toast.makeText(this, "Tidak Boleh Kosong", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val hasil = 2 * 3.14 * jari.toFloat()
+
+        binding.hasilPerhitunganKel.text = getString(R.string.hasil, hasil)
+
     }
 
 }
